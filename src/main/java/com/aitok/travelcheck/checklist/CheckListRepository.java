ece3b4412a0a0743c59e4c9e1ffb639a5088d966
sejin <sejin3510@gmail.com>
2024-05-09 14:37:45 +0900

    Revert "[ADDING] JPA 연동"
    This reverts commit 241e0432e6cf3a6d5721b9974e4aaf2eaf25da06.

--- src/main/java/com/aitok/travelcheck/checklist/CheckListRepository.java
+++
@@ -1,6 +0,0 @@
-package com.aitok.travelcheck.checklist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CheckListRepository extends JpaRepository<CheckList, Integer> {
-}
